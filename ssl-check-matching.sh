fb955f4330e611f13b96fc36ffb0338b41b12a3f
Oleg Podgaisky <oleg-podgaisky@yandex.ru>
2018-08-09 13:24:46 +0300

    Error handler was separated from code

--- ssl-check-matching.sh
+++ ssl-check-matching.sh
@@ -4,8 +4,8 @@
 # Source:       https://github.com/o-pod/security
 #
 # Description:  Checking public and private keys for valid and matching
-# Version:      0.1.0
-# Date:         Jun 2018
+# Version:      0.2.0
+# Date:         Aug 2018
 # Depends:      OpenSSL
 #
 # Author:       Oleg Podgaisky (o-pod)
@@ -15,10 +15,22 @@
 # Output:       0 - certificate and private key are match
 #               1 - certificate and private key are NOT match
 #               2 - error
-#==============================================================================
+#======================================================================
 
 params=$(echo "${*}" | sed 's/[\t ]\{1,\}/ /g' | sed 's/^[ ]\{1,\}//' | sed 's/[ ]\{1,\}$//')
 verbose=$(echo " "${params}" " | grep -o " \-v ")
+
+
+### Exit processing
+#
+function myExit {
+    if [ -n "${verbose}" ]; then
+        echo "${2}"    ### Exit message
+    else
+        echo "${1}"    ### Exit code:    0 - True,    1 - False,    2 - Error
+    fi
+    exit ${1}
+}
 
 
 ### Test parameters of the commandline
@@ -26,34 +38,18 @@ verbose=$(echo " "${params}" " | grep -o " \-v ")
 files=$(echo " "${params}" " | sed ' s/-[a-z] / /gi' | sed 's/[\t ]\{1,\}/ /g')
 file1=$(echo ${files} | cut -d" " -f1)
 file2=$(echo ${files} | cut -d" " -f2)
-
 if [ "${file1}" == "${file2}" ]; then
-  if [ -n "${verbose}" ]; then
-    echo "ERROR: You should specify 2 not same files as parameters"
-  else
-    echo 2
-  fi
-  exit 2
+    myExit 2 "ERROR: You should specify 2 not same files as parameters"
 fi
 
 
 ### Test the files for exist
 #
 if [ ! -f "${file1}" ]; then
-  if [ -n "${verbose}" ]; then
-    echo "ERROR: File ${file1} is not exist or access denied"
-  else
-    echo 2
-  fi
-  exit 2
+    myExit 2 "ERROR: File ${file1} is not exist or access denied"
 fi
 if [ ! -f "${file2}" ]; then
-  if [ -n "${verbose}" ]; then
-    echo "ERROR: File ${file2} is not exist or access denied"
-  else
-    echo 2
-  fi
-  exit 2
+    myExit 2 "ERROR: File ${file2} is not exist or access denied"
 fi
 
 
@@ -62,31 +58,21 @@ fi
 cert_from_file=$(openssl x509 -noout -modulus -in ${file1} 2>&1)
 cert_from_file_test=$(echo ${cert_from_file} | grep "unable to load certificate")
 if [ -n "${cert_from_file_test}" ]; then
-  cert_from_file=$(openssl x509 -noout -modulus -in ${file2} 2>&1)
-  cert_from_file_test=$(echo ${cert_from_file} | grep "unable to load certificate")
-  if [ -n "${cert_from_file_test}" ]; then
-    if [ -n "${verbose}" ]; then
-      echo "ERROR: Not the ${file1} nor the ${file2} are valid certificate"
-    else
-      echo 2
+    cert_from_file=$(openssl x509 -noout -modulus -in ${file2} 2>&1)
+    cert_from_file_test=$(echo ${cert_from_file} | grep "unable to load certificate")
+    if [ -n "${cert_from_file_test}" ]; then
+        myExit 2 "ERROR: Not the ${file1} nor the ${file2} are valid certificate"
     fi
-    exit 2
-  fi
 fi
 
 key_from_file=$(openssl rsa -noout -modulus -in ${file1} 2>&1)
 key_from_file_test=$(echo ${key_from_file} | grep "unable to load Private Key")
 if [ -n "${key_from_file_test}" ]; then
-  key_from_file=$(openssl rsa -noout -modulus -in ${file2} 2>&1)
-  key_from_file_test=$(echo ${key_from_file} | grep "unable to load Private Key")
-  if [ -n "${key_from_file_test}" ]; then
-    if [ -n "${verbose}" ]; then
-      echo "ERROR: Not the ${file1} nor the ${file2} are valid private key"
-    else
-      echo 2
+    key_from_file=$(openssl rsa -noout -modulus -in ${file2} 2>&1)
+    key_from_file_test=$(echo ${key_from_file} | grep "unable to load Private Key")
+    if [ -n "${key_from_file_test}" ]; then
+        myExit 2 "ERROR: Not the ${file1} nor the ${file2} are valid private key"
     fi
-    exit 2
-  fi
 fi
 
 
@@ -95,18 +81,8 @@ fi
 cert_md5=$(echo ${cert_from_file} | openssl md5)
 key_md5=$(echo ${key_from_file} | openssl md5)
 if [ "${cert_md5}" != "${key_md5}" ]; then
-  if [ -n "${verbose}" ]; then
-    echo "The certificate and the private key are NOT matching"
-  else
-    echo 1
-    exit 1
-  fi
+    myExit 1 "PROBLEM: The certificate and the private key are NOT matching"
 else
-  if [ -n "${verbose}" ]; then
-    echo "The certificate and the private key are matching"
-  else
-    echo 0
-    exit 0
-  fi
+    myExit 0 "SUCCESS: The certificate and the private key are matching"
 fi
 
--- ssl-check-revoc.sh
+++ ssl-check-revoc.sh
@@ -4,8 +4,8 @@
 # Source:       https://github.com/o-pod/security
 #
 # Description:  Checking SSL certificates for revocation using CRL
-# Version:      0.1.0
-# Date:         Jun 2018
+# Version:      0.2.0
+# Date:         Aug 2018
 # Depends:      OpenSSL, Wget
 #
 # Author:       Oleg Podgaisky (o-pod)
@@ -15,59 +15,49 @@
 # Output:       0 - the certificate is not revoked
 #               1 - the certificate is revoked
 #               2 - error
-#==============================================================================
+#=================================================================
 
 path="/tmp"
 verbose=$(echo " "${*}" " | grep -o " \-v ")
 file=$(echo " "${*}" " | grep -o " \-f ")
 
 
-### Get certificate from FILE
+### Exit processing
 #
+function myExit {
+    if [ -n "${verbose}" ]; then
+        echo "${2}"    ### Exit message
+    else
+        echo "${1}"    ### Exit code:    0 - True,    1 - False,    2 - Error
+    fi
+    exit ${1}
+}
+
+
+### Get a certificate
+#
+# from FILE:
 if [ -n "${file}" ]; then
-  crt_file=$(echo ${*} | sed 's/\-[a-z]//gi' | sed 's/ //g')
-  if [ ! -f "${crt_file}" ]; then
-    if [ -n "${verbose}" ]; then
-      echo "ERROR: File is not exist"
-    else
-      echo 2
+    crt_file=$(echo ${*} | sed 's/\-[a-z]//gi' | sed 's/ //g')
+    if [ ! -f "${crt_file}" ]; then
+        myExit 2 "ERROR: File ${crt_file} is not exist"
     fi
-    exit 2
-  fi
-
-
-### Get certificate from DOMAIN
-#
+# from DOMAIN:
 else
-  domain=$(echo ${*} | sed 's/\-[a-z]//gi' | sed 's/ //g')
-  if [ -z "${domain}" ]; then
-    if [ -n "${verbose}" ]; then
-      echo "ERROR: Domain is not defined"
-    else
-      echo 2
+    domain=$(echo ${*} | sed 's/\-[a-z]//gi' | sed 's/ //g')
+    if [ -z "${domain}" ]; then
+        myExit 2 "ERROR: Domain is not defined"
     fi
-    exit 2
-  fi
-  domain_ping=$(ping -c1 ${domain} 2>&1 | grep -v "unknown host")
-  if [ -z "${domain_ping}" ]; then
-    if [ -n "${verbose}" ]; then
-      echo "ERROR: Unknown domain ${domain}"
-    else
-      echo 2
+    domain_ping=$(ping -c1 ${domain} 2>&1 | grep -v "unknown host")
+    if [ -z "${domain_ping}" ]; then
+        myExit 2 "ERROR: Unknown domain ${domain}"
     fi
-    exit 2
-  fi
-  crt_file="${path}/${domain}.crt"
-  echo -n | openssl s_client -connect ${domain}:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${crt_file}
-  crt_file_size=$(wc -c < "${crt_file}")
-  if [ $crt_file_size -lt 100 ]; then
-    if [ -n "${verbose}" ]; then
-      echo "ERROR: Failed to get certificate from domain ${domain}"
-    else
-      echo 2
+    crt_file="${path}/${domain}.crt"
+    echo -n | openssl s_client -connect ${domain}:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${crt_file}
+    crt_file_size=$(wc -c < "${crt_file}")
+    if [ $crt_file_size -lt 100 ]; then
+        myExit 2 "ERROR: Failed to get certificate from domain ${domain}"
     fi
-    exit 2
-  fi
 fi
 
 
@@ -76,12 +66,10 @@ fi
 crt_file_data=$(openssl x509 -noout -modulus -in ${crt_file} 2>&1)
 crt_file_data_test=$(echo ${crt_file_data} | grep "unable to load certificate")
 if [ -n "${crt_file_data_test}" ]; then
-  if [ -n "${verbose}" ]; then
-    echo "ERROR: The certificate is NOT valid"
-  else
-    echo 2
-  fi
-  exit 2
+    if [ -n "${domain}" ]; then
+        rm ${crt_file} 2>/dev/null
+    fi
+    myExit 2 "ERROR: The certificate is NOT valid"
 fi
 
 
@@ -89,64 +77,56 @@ fi
 #
 crt_serial=$(openssl x509 -in ${crt_file} -noout -serial | cut -d"=" -f2)
 if [ -n "${verbose}" ]; then
-  echo "=== Certificate details ==="
-  crt_subject=$(openssl x509 -in ${crt_file} -noout -subject | sed 's/^subject=//')
-  echo "Subject: ${crt_subject}"
-  crt_issuer=$(openssl x509 -in ${crt_file} -noout -issuer | sed 's/^issuer=//')
-  echo "Issuer: ${crt_issuer}"
-  echo "Serial: ${crt_serial}"
-  crt_startdate=$(openssl x509 -in ${crt_file} -noout --startdate | sed 's/^notBefore=//')
-  echo "Start date: ${crt_startdate}"
-  crt_enddate=$(openssl x509 -in ${crt_file} -noout --enddate | sed 's/^notAfter=//')
-  echo "End date: ${crt_enddate}"
-  ocsp_url=$(openssl x509 -in ${crt_file} -noout -ocsp_uri)
-  echo "OCSP server: ${ocsp_url}"
+    echo "=== Certificate details ==="
+    crt_subject=$(openssl x509 -in ${crt_file} -noout -subject | sed 's/^subject=//')
+    echo "Subject: ${crt_subject}"
+    crt_issuer=$(openssl x509 -in ${crt_file} -noout -issuer | sed 's/^issuer=//')
+    echo "Issuer: ${crt_issuer}"
+    echo "Serial: ${crt_serial}"
+    crt_startdate=$(openssl x509 -in ${crt_file} -noout -startdate | sed 's/^notBefore=//')
+    echo "Start date: ${crt_startdate}"
+    crt_enddate=$(openssl x509 -in ${crt_file} -noout -enddate | sed 's/^notAfter=//')
+    echo "End date: ${crt_enddate}"
+    ocsp_url=$(openssl x509 -in ${crt_file} -noout -ocsp_uri)
+    echo "OCSP server: ${ocsp_url}"
 fi
 
 
 ### Download CRL
 #
 if [ -n "${verbose}" ]; then
-  echo "=== CRL ==="
+    echo "=== CRL ==="
 fi
 crl_url=$(openssl x509 -noout -text -in ${crt_file} | grep -o 'http://.\{1,\}\.crl$')
 if [ -z "${crl_url}" ]; then
-  if [ -n "${verbose}" ]; then
-    echo "ERROR: CRL is not present"
-  else
-    echo 2
-  fi
-  exit 2
+    if [ -n "${domain}" ]; then
+        rm ${crt_file} 2>/dev/null
+    fi
+    myExit 2 "ERROR: CRL is not present"
 fi
 if [ -n "${verbose}" ]; then
-  echo "CRL from: ${crl_url}"
+    echo "CRL from: ${crl_url}"
 fi
 crl_file=$(echo ${crl_url} | grep -o '[^\/]\{1,\}$')
 wget --quiet ${crl_url} -O ${path}/${crl_file}
 if [ -n "${verbose}" ]; then
-  echo "CRL was saved to: ${path}/${crl_file}"
+    echo "CRL was saved to: ${path}/${crl_file}"
 fi
 
 
 ### Look for serial of the certificate in the CRL
 #
 if [ -n "${verbose}" ]; then
-  echo "=== REVOCATION STATUS ==="
+    echo "=== REVOCATION STATUS ==="
 fi
 result=$(openssl crl -inform DER -text -in ${path}/${crl_file} | grep "${crt_serial}")
+rm ${path}/${crl_file} 2>/dev/null
+if [ -n "${domain}" ]; then
+    rm ${crt_file} 2>/dev/null
+fi
 if [ -z "${result}" ]; then
-  if [ -n "${verbose}" ]; then
-    echo "SUCCESS: The certificate is not in revocation list"
-  else
-    echo 0
-    exit 0
-  fi
+    myExit 0 "SUCCESS: The certificate is not in revocation list"
 else
-  if [ -n "${verbose}" ]; then
-    echo "PROBLEM: The certificate is in revocation list"
-  else
-    echo 1
-    exit 1
-  fi
+    myExit 1 "PROBLEM: The certificate is in revocation list"
 fi
 
